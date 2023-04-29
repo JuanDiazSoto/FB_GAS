4dbc0064c271f891e3a4244965caafd0146db613
Juan Díaz Soto <jfds.205@gmail.com>
2023-04-29 12:07:04 -0400

    Create css.js

---
+++ BASE_WEB_SCRIPT/css.js
@@ -0,0 +1,15 @@
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<!-- Iconos-->
+<script src="https://kit.fontawesome.com/75357fa980.js" crossorigin="anonymous"></script>
+
+<!-- Data Table CSS-->
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css">
+<link rel="stylesheet" type="text/css"
+  href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css" />
+
+<!-- Animacion CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
